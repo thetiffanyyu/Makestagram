0cf8f725f0a8f06f410e05b4f3651f38ffc33a4c
thetiffanyyu <thetiffanyyu@gmail.com>
2016-06-27 14:24:41 -0700

    Upload Photo Functions

---
+++ Makestagram/Views/Timeline/PostTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PostTableViewCell.swift
+//  Makestagram
+//
+//  Created by Tiffany Yu on 6/27/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    // ...
+    
+}
